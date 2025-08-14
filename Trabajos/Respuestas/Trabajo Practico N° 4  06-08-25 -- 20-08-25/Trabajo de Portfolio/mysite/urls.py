a08837bf611dc52f93224097d0a60be9a04a36ac
Thomas-Rossel <thomy17fr@gmail.com>
2025-08-14 15:44:15 -0300

    Fixeando Bugs

--- Trabajos/Respuestas/Trabajo Practico N° 4  06-08-25 -- 20-08-25/Trabajo de Portfolio/mysite/urls.py
+++ Trabajos/Respuestas/Trabajo Practico N° 4  06-08-25 -- 20-08-25/Trabajo de Portfolio/mysite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Portfolio.urls')),
+    path('portfolio/', include('Portfolio.urls')),
     path('blog/', include('Blog.urls')),
 ]
