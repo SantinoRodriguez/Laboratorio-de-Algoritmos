bafada776654de4e8b876fb8986714079f32d3f4
SantinoRodriguez <santinorofu@gmail.com>
2025-08-15 18:06:23 -0300

    Base y Barras del Blog

--- Trabajos/Respuestas/Trabajo Practico N° 4  06-08-25 -- 20-08-25/Trabajo de Portfolio/Blog/views.py
+++ Trabajos/Respuestas/Trabajo Practico N° 4  06-08-25 -- 20-08-25/Trabajo de Portfolio/Blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Página del Blog")
+    return render(request, 'blog/index.html')
