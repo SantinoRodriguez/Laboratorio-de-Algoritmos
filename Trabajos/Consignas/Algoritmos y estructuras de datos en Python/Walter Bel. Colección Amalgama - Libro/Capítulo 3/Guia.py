629e973ddad37f0ebbdc8bd04fad338e64627ff8
Santino Luciano Rodriguez Fuchs <santinorofu@gmail.com>
2025-10-02 00:03:51 -0300

    Capitulo 3 del Libro

---
+++ Trabajos/Consignas/Algoritmos y estructuras de datos en Python/Walter Bel. Colección Amalgama - Libro/Capítulo 3/Guia.py
@@ -0,0 +1,85 @@
+# 1 Fragmento de código condicional:
+if num > 10:
+    print('es mayor que 10')
+    print('su cuadrado es 100')
+    aux = num % 2
+else:
+    print('es menor que 10')
+    print( 'su cubo es 1000')
+    aux = num // 2
+    print(aux)
+    producto = num * 2
+
+# 2 Fragmento de código condicional:
+if num > 10:
+    print('es mayor que 10')
+    print('su cuadrado es 100')
+    aux = num % 2
+    if(num % 2 = 0):
+        print( 'es divisible por 2')
+    else:
+        print('es divisible por 2')
+        num = num + 1
+        aux = num * num * num
+else:
+    print('es menor que 10')
+    print('su cubo es 1000')
+    aux = num // 2
+    print(aux)
+    producto = num * 2
+    if(producto % 3 = 0)
+        print('es divisible por 3')
+
+# 3 Fragmento de código de ciclo:
+for i in range(0, n):
+    print('incio ciclo j')
+    for j in range(o, m):
+        print('incio ciclo k')
+        for k in range(0, 3):
+            print(lista[k])
+
+# 4 Fragmento de código de ciclo:
+for i in range(0, n):
+    if(aux is not None):
+        print('incio ciclo j')
+        for j in range(0, m):
+            print(num + (j * valor))
+    else:
+        print('incio ciclo k')
+        for k in range(0, 1000):
+            print(aux[k] + num)
+
+# 5 Fragmento de código condicional:
+if num > 10:
+    print('es mayor que 10')
+    print('su cuadrado es 100')
+    aux = num % 2
+    print('tabla de multiplicar')
+    for i in range(1, 16):
+        print(i, num * i)
+else:
+    print('es menor que 10')
+    print('su cubo es 1000')
+    aux = num // 2
+    print(aux)
+    producto = num * 2
+    if(producto % 3 = 0):
+        print( 'es divisible por 3')
+
+# 6 Fragmento de código:
+numero = int(input('ingrese un número'))
+while (numero != 0) and (len(lista) < 10000):
+    lista.append(numero)
+    numero = int(input('ingrese número'))
+
+for i in range(0, len(lista)):
+    print(lista[i])
+
+# 7 Búsqueda binaria iterativa:
+while (p <= u) and (pos = -1):
+    med = (p + u) // 2
+    pos = med
+    if(x > lista[med]):
+    p med + 1
+    else:
+    p med 1
